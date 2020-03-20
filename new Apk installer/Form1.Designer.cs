8a81f1612fd401574cecfcff5fb947269c1d6091
m-nemati <m.nemati2002@gmail.com>
2020-03-20 15:21:13 +0330

    Work = 001

---
+++ new Apk installer/Form1.Designer.cs
@@ -0,0 +1,187 @@
+﻿namespace new_Apk_installer
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblAbout = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lblAppInfo = new System.Windows.Forms.Label();
+            this.btnInstall = new System.Windows.Forms.Button();
+            this.btnInfo = new System.Windows.Forms.Button();
+            this.btnRename = new System.Windows.Forms.Button();
+            this.btnSelect = new System.Windows.Forms.Button();
+            this.rtbInfo = new System.Windows.Forms.RichTextBox();
+            this.ofdApk = new System.Windows.Forms.OpenFileDialog();
+            this.groupBox1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblAbout
+            // 
+            this.lblAbout.AutoSize = true;
+            this.lblAbout.Location = new System.Drawing.Point(175, 11);
+            this.lblAbout.Name = "lblAbout";
+            this.lblAbout.Size = new System.Drawing.Size(153, 17);
+            this.lblAbout.TabIndex = 0;
+            this.lblAbout.Text = "مصطفی نعمتی - حمیدرضا ضیایی";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.panel1);
+            this.groupBox1.Controls.Add(this.btnInstall);
+            this.groupBox1.Controls.Add(this.btnInfo);
+            this.groupBox1.Controls.Add(this.btnRename);
+            this.groupBox1.Controls.Add(this.btnSelect);
+            this.groupBox1.Location = new System.Drawing.Point(13, 30);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.groupBox1.Size = new System.Drawing.Size(500, 146);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Apk Installer";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.panel1.Controls.Add(this.lblAppInfo);
+            this.panel1.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.panel1.Location = new System.Drawing.Point(106, 26);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(288, 54);
+            this.panel1.TabIndex = 4;
+            this.panel1.Visible = false;
+            // 
+            // lblAppInfo
+            // 
+            this.lblAppInfo.BackColor = System.Drawing.Color.Black;
+            this.lblAppInfo.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblAppInfo.ForeColor = System.Drawing.SystemColors.Window;
+            this.lblAppInfo.Location = new System.Drawing.Point(2, 3);
+            this.lblAppInfo.Name = "lblAppInfo";
+            this.lblAppInfo.Padding = new System.Windows.Forms.Padding(0, 6, 0, 0);
+            this.lblAppInfo.Size = new System.Drawing.Size(284, 48);
+            this.lblAppInfo.TabIndex = 0;
+            this.lblAppInfo.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblAppInfo.Visible = false;
+            // 
+            // btnInstall
+            // 
+            this.btnInstall.Location = new System.Drawing.Point(253, 83);
+            this.btnInstall.Name = "btnInstall";
+            this.btnInstall.Size = new System.Drawing.Size(241, 56);
+            this.btnInstall.TabIndex = 3;
+            this.btnInstall.Text = "Install";
+            this.btnInstall.UseVisualStyleBackColor = true;
+            this.btnInstall.Click += new System.EventHandler(this.btnInstall_Click);
+            // 
+            // btnInfo
+            // 
+            this.btnInfo.Location = new System.Drawing.Point(395, 26);
+            this.btnInfo.Name = "btnInfo";
+            this.btnInfo.Size = new System.Drawing.Size(99, 55);
+            this.btnInfo.TabIndex = 2;
+            this.btnInfo.Text = "Quick Info";
+            this.btnInfo.UseVisualStyleBackColor = true;
+            this.btnInfo.Click += new System.EventHandler(this.btnInfo_Click);
+            // 
+            // btnRename
+            // 
+            this.btnRename.Location = new System.Drawing.Point(6, 83);
+            this.btnRename.Name = "btnRename";
+            this.btnRename.Size = new System.Drawing.Size(242, 56);
+            this.btnRename.TabIndex = 1;
+            this.btnRename.Text = "Rename";
+            this.btnRename.UseVisualStyleBackColor = true;
+            this.btnRename.Click += new System.EventHandler(this.btnRename_Click);
+            // 
+            // btnSelect
+            // 
+            this.btnSelect.Location = new System.Drawing.Point(6, 26);
+            this.btnSelect.Name = "btnSelect";
+            this.btnSelect.Size = new System.Drawing.Size(99, 55);
+            this.btnSelect.TabIndex = 0;
+            this.btnSelect.Text = "Add Apks";
+            this.btnSelect.UseVisualStyleBackColor = true;
+            this.btnSelect.Click += new System.EventHandler(this.btnSelect_Click);
+            // 
+            // rtbInfo
+            // 
+            this.rtbInfo.BackColor = System.Drawing.SystemColors.Window;
+            this.rtbInfo.Font = new System.Drawing.Font("IRANSans", 8.999999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.rtbInfo.Location = new System.Drawing.Point(13, 184);
+            this.rtbInfo.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.rtbInfo.Name = "rtbInfo";
+            this.rtbInfo.ReadOnly = true;
+            this.rtbInfo.Size = new System.Drawing.Size(500, 151);
+            this.rtbInfo.TabIndex = 2;
+            this.rtbInfo.Text = "";
+            // 
+            // ofdApk
+            // 
+            this.ofdApk.FileName = "openFileDialog1";
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(525, 341);
+            this.Controls.Add(this.rtbInfo);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.lblAbout);
+            this.Font = new System.Drawing.Font("IRANSans", 8.249999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.MaximumSize = new System.Drawing.Size(541, 380);
+            this.MinimumSize = new System.Drawing.Size(541, 380);
+            this.Name = "frmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Apk Installer 2.0";
+            this.groupBox1.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblAbout;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RichTextBox rtbInfo;
+        private System.Windows.Forms.Button btnInstall;
+        private System.Windows.Forms.Button btnInfo;
+        private System.Windows.Forms.Button btnRename;
+        private System.Windows.Forms.Button btnSelect;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.OpenFileDialog ofdApk;
+        private System.Windows.Forms.Label lblAppInfo;
+    }
+}
+
---
+++ new Apk installer/Form1.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Csharp.Apk_Reader;
+using System.IO;
+using Microsoft.VisualBasic.FileIO;
+using SharpAdbClient;
+
+namespace new_Apk_installer
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        string apkSelected;
+        string apkSelectedFileName;
+        ApkInfo file_info, file_info2;
+        bool fileIsSelected = false;
+        
+        /* ******* Impelement of OpenFile Dialog Function ******** */
+        public bool openFileDialogeFunc()
+        {
+            ofdApk.DefaultExt = "*.apk";
+            ofdApk.AddExtension = true;
+            ofdApk.Multiselect = true;
+            ofdApk.Filter = "Android Application Files|*.apk";
+            ofdApk.Title = "Select Your APK File";
+            ofdApk.FileName = "Choose Files...";
+            bool retFlag = ofdApk.ShowDialog() == DialogResult.OK;
+            return retFlag;
+        }
+        private void btnSelect_Click(object sender, EventArgs e)
+        {
+            rtbInfo.Text = "";
+            bool flag = openFileDialogeFunc();
+            if (flag)
+            {
+                fileIsSelected = true;
+                foreach (string file in ofdApk.FileNames)
+                {
+                    apkSelected = ofdApk.FileName;
+                    apkSelectedFileName = ofdApk.SafeFileName;
+                    var readApk = new Apk_Reader();
+
+                    file_info = readApk.Get_File_Info(file);
+                    panel1.Visible = true;
+                    lblAppInfo.Visible = true;
+                    lblAppInfo.Text = "APK Name: " + file_info.apkName + "\n" + "APK Version: " + file_info.versionName + "\n";
+                }
+
+                /*string[] apkSelected2 = ofdApk.FileNames;
+                int i = 0;
+                foreach (string str in apkSelected2)
+                {
+                    i++;
+                    long lengthApk = new System.IO.FileInfo(str).Length;
+                    string retSize = humanReadableConvert(lengthApk);
+                    rtbInfo.Text = rtbInfo.Text + "\nSuccesful Select[" + i + "]" + ":\n" +
+                        "\t>> Apk Path: " + str + "\n" +
+                        "- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - ";
+                }*/
+            }
+
+        }
+
+        private void btnInfo_Click(object sender, EventArgs e)
+        {
+            AdbServer server = new AdbServer();
+            var result = server.StartServer("adb.exe", restartServerIfNewer: false);
+            var devices = AdbClient.Instance.GetDevices();
+            int i = 1;
+            rtbInfo.Text = "";
+            foreach (var device in devices)
+            {
+
+                rtbInfo.Text = rtbInfo.Text + "\n" + "Device " + i + ":\n" +
+                    "\t>> Phone Name: " + device.Name + "\n" +
+                    "\t>> Phone Model: " + device.Model + "\n" +
+                    "\t>> Phone Product: " + device.Product + "\n" +
+                    "\t>> Phone Serial: " + device.Serial;
+                i++;
+                rtbInfo.Text = rtbInfo.Text + "\n- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - ";
+            }
+            
+        }
+
+        private void btnInstall_Click(object sender, EventArgs e)
+        {
+            rtbInfo.Text = "";
+        }
+
+        private void btnRename_Click(object sender, EventArgs e)
+        {
+            if (!fileIsSelected)
+            {
+                rtbInfo.Text = "";
+                rtbInfo.Text = "Please Select a Apk File First";
+            }
+            else
+            {
+                rtbInfo.Text = "";
+                int i = 0;
+                foreach (string file in ofdApk.FileNames)
+                {
+                    apkSelected = ofdApk.FileNames[i];
+                    apkSelectedFileName = ofdApk.SafeFileNames[i];
+                    i += 1;
+                    var readApk2 = new Apk_Reader();
+
+                    file_info2 = readApk2.Get_File_Info(file);
+
+                    if (apkSelectedFileName != file_info2.apkName + " " + file_info2.versionName + ".apk")
+                    {
+                        string tempStr = Path.GetDirectoryName(apkSelected);
+
+                        if (File.Exists(tempStr + "\\" + file_info2.apkName + " " + file_info2.versionName + ".apk"))
+                        {
+                            rtbInfo.Text = "A File With This Apk Name Exist IN Your Computer, Please Rename That File And Try Agian.";
+                            rtbInfo.Text = rtbInfo.Text + "\n- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - ";
+                        }
+                        else
+                        {
+                            if (!File.Exists(apkSelected))
+                            {
+                                rtbInfo.Text = "You Already Changed The Apk Name.";
+                                rtbInfo.Text = rtbInfo.Text + "\n- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - ";
+                            }
+                            else
+                            {
+                                FileSystem.RenameFile(apkSelected, file_info2.apkName + " " + file_info2.versionName + ".apk");
+                                string apkPathPrint = Path.GetDirectoryName(apkSelected);
+                                rtbInfo.Text = rtbInfo.Text + "\n" + "Successful:";
+                                rtbInfo.Text = rtbInfo.Text + "\n\t>> Apk Path: " + apkPathPrint + "\\" + file_info2.apkName + " " + file_info2.versionName + ".apk";  
+                                rtbInfo.Text = rtbInfo.Text + "\n\t>> APK Renamed From \"" + apkSelectedFileName + "\" To \"" + file_info2.apkName + " " + file_info2.versionName + ".apk\"";
+
+                                long length = new System.IO.FileInfo(tempStr + "\\" + file_info2.apkName + " " + file_info2.versionName + ".apk").Length;
+                                string ffSize = humanReadableConvert(length);
+                                rtbInfo.Text = rtbInfo.Text + "\n\t>> APK Size: " + ffSize + "\n"; //length.ToString() + " Bytes\n";
+                                rtbInfo.Text = rtbInfo.Text + "- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - ";
+                            }
+
+                        }
+
+                    }
+                    else
+                    {
+                        rtbInfo.Text = rtbInfo.Text + "\n" + apkSelectedFileName + " >> The Original APK File Name IS The Same AS The Previous One, No Need TO Change The APK Name.";
+                        rtbInfo.Text = rtbInfo.Text + "\n- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - ";
+                    }
+
+                }
+            }
+       
+        }
+
+        /* ******* Impelement of humanReadableConvert Function ******** */
+        public string humanReadableConvert(double fSize)
+        {
+            string[] sizes = { "B", "KB", "MB", "GB", "TB" };
+            //double len = new FileInfo(filename).Length;
+            int order = 0;
+            while (fSize >= 1024 && order < sizes.Length - 1)
+            {
+                order++;
+                fSize = fSize / 1024;
+            }
+
+            // Adjust the format string to your preferences. For example "{0:0.#}{1}" would
+            // show a single decimal place, and no space.
+            string resultSize = String.Format("{0:0.##} {1}", fSize, sizes[order]);
+
+            return resultSize;
+        }
+    }
+}
